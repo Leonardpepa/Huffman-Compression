6a99938fc3b001934175cf8de29f5cb0e5a244da
Leonardpepa <leopepp4@gmail.com>
2023-12-21 04:00:54 +0200

    improved cli api

--- main.go
+++ main.go
@@ -5,47 +5,38 @@ import (
 	"huffmanCompression/huffman"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 
 	c := flag.Bool("c", false, "Specify a file to compress")
 	d := flag.Bool("d", false, "Specify a file to decompress")
+	h := flag.Bool("h", false, "Usage")
 
 	flag.Parse()
 
+	if *h {
+		usage()
+	}
+
 	if *c == false && *d == false {
-		log.Fatal(`
-	Usage: huffman.exe [OPTIONS]... [FILE] ~1 file
-	[OPTIONS]:
-	-c FILE ~file to compress
-	-d FILE ~file to decompress
-`)
+		usage()
 	}
 
 	if *c && *d {
-		log.Fatal(`
-	
-	Usage: huffman.exe [OPTIONS]... [FILE] ~1 file
-	[OPTIONS]:
-	-c FILE ~file to compress
-	-d FILE ~file to decompress
-`)
+		log.Fatal("You can choose either compress or decompress. Try '--help' for more information.")
 	}
 
 	if len(flag.Args()) != 1 {
-		log.Fatal(`
-	Usage: huffman.exe [OPTIONS]... [FILE]
-	[OPTIONS]:
-	-c FILE ~file to compress
-	-d FILE ~file to decompress
-`)
+		log.Fatal("Please provide a file. Try '--help' for more information.")
 	}
 
 	if *c {
 		file := openFile(flag.Args()[0])
 
-		err := huffman.Encode(file, "output.hf")
+		err := huffman.Encode(file, filepath.Base(file.Name())+".hf")
 
 		if err != nil {
 			log.Fatal(err)
@@ -53,12 +44,25 @@ func main() {
 
 	} else if *d {
 		file := openFile(flag.Args()[0])
-		err := huffman.Decode(file, "decoded.txt")
+		err := huffman.Decode(file, strings.Split(filepath.Base(file.Name()), ".hf")[0])
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
+}
+
+func usage() {
+	log.Fatal(`
+Usage: huffman.exe [OPTIONS]... [FILE]
+[OPTIONS]:
+  -c FILE ~file to compress
+  -d FILE ~file to decompress
+[FILE]:
+  the file specified is the file to compress or decompress,
+  when compressed the file will be saved with a .hf extension,
+  when decompressed the file will be saved as a copy of the original file
+`)
 }
 
 // TODO create cli api
