ab196477876b1a294890536f0fd537d01013dc7d
Leonardpepa <leopepp4@gmail.com>
2023-12-21 09:00:05 +0200

    added more error handling in utf8 decoding

--- bitstream/bitReader.go
+++ bitstream/bitReader.go
@@ -37,7 +37,7 @@ func (reader *Reader) BitPosition() int {
 	return reader.count
 }
 
-func (reader *Reader) getBitAsBool(num byte, i int) bool {
+func (reader *Reader) getBitAt(num byte, i int) bool {
 	if i < 0 || i > 7 {
 		log.Fatal("Error index must be in bounds [0-7]")
 	}
@@ -57,7 +57,7 @@ func (reader *Reader) Read() bool {
 		log.Fatal("No more bits to read. Abort")
 	}
 
-	bit := reader.getBitAsBool(reader.data[reader.offset], 7-reader.count)
+	bit := reader.getBitAt(reader.data[reader.offset], 7-reader.count)
 
 	reader.count++
 	if reader.count == 8 {
@@ -75,20 +75,32 @@ func (reader *Reader) ReadChar() (rune, error) {
 	if err != nil {
 
 	}
+
+	bit1 := reader.getBitAt(byte1, 7)
 	//0xxxxxxx
-	if reader.getBitAsBool(byte1, 7) == false {
+	if bit1 == false {
 		r, _ := utf8.DecodeRune([]byte{byte1})
 		return r, nil
 	}
 
+	bit2 := reader.getBitAt(byte1, 6)
+	bit3 := reader.getBitAt(byte1, 5)
+
 	// 110xxxxx 10xxxxxx
-	if reader.getBitAsBool(byte1, 7) &&
-		reader.getBitAsBool(byte1, 6) &&
-		reader.getBitAsBool(byte1, 5) == false {
+	if bit1 && bit2 && bit3 == false {
 		byte2, err := reader.ReadByte()
 
 		if err != nil {
+
 			return 0, err
+		}
+
+		bit1 = reader.getBitAt(byte2, 7)
+		bit2 = reader.getBitAt(byte2, 6)
+
+		if bit1 == false || bit2 == true {
+			log.Printf("%08b, %08b", byte1, byte2)
+			return 0, fmt.Errorf("error while decoding 110xxxxx 10xxxxxx utf8 rune")
 		}
 
 		r, _ := utf8.DecodeRune([]byte{byte1, byte2})
@@ -96,21 +108,37 @@ func (reader *Reader) ReadChar() (rune, error) {
 
 	}
 
+	bit4 := reader.getBitAt(byte1, 4)
 	// 1110xxxx 10xxxxxx 10xxxxxx
-	if reader.getBitAsBool(byte1, 7) &&
-		reader.getBitAsBool(byte1, 6) &&
-		reader.getBitAsBool(byte1, 5) &&
-		reader.getBitAsBool(byte1, 4) == false {
+	if bit1 && bit2 && bit3 && bit4 == false {
 		byte2, err := reader.ReadByte()
 
 		if err != nil {
 			return 0, err
 		}
+
+		bit1 = reader.getBitAt(byte2, 7)
+		bit2 = reader.getBitAt(byte2, 6)
+
+		if bit1 == false || bit2 == true {
+			log.Printf("%08b, %08b, error in the second byte", byte1, byte2)
+			return 0, fmt.Errorf("error while decoding 1110xxxx 10xxxxxx 10xxxxxx utf8 rune")
+		}
+
 		byte3, err := reader.ReadByte()
 
 		if err != nil {
 			return 0, err
 		}
+
+		bit1 = reader.getBitAt(byte3, 7)
+		bit2 = reader.getBitAt(byte3, 6)
+
+		if bit1 == false || bit2 == true {
+			log.Printf("%08b, %08b, error in the third byte", byte1, byte2)
+			return 0, fmt.Errorf("error while decoding 1110xxxx 10xxxxxx 10xxxxxx utf8 rune")
+		}
+
 		r, _ := utf8.DecodeRune([]byte{byte1, byte2, byte3})
 		return r, nil
 
