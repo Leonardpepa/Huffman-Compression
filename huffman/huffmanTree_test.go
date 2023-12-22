fce37d354f3037aa16699ed69b71a9e785177928
Leonardpepa <leopepp4@gmail.com>
2023-12-22 21:09:54 +0200

    created test for pririty queue

---
+++ huffman/huffmanTree_test.go
@@ -0,0 +1,34 @@
+package huffman
+
+import (
+	"container/heap"
+	"testing"
+)
+
+func TestCreatePriorityQueue(t *testing.T) {
+	t.Run("create priority queue from frequency table", func(t *testing.T) {
+		frequencies := map[rune]uint64{
+			'L': 42,
+			'K': 7,
+			'M': 24,
+			'C': 32,
+			'U': 37,
+			'Z': 2,
+			'D': 42,
+			'E': 120,
+		}
+
+		pq := CreatePriorityQueue(frequencies)
+
+		prev := uint64(0)
+		for pq.Len() > 0 {
+			item := heap.Pop(&pq).(*TreeNode)
+
+			if item.Weight < prev {
+				t.Errorf("wrong item popped. last was %d but got %d", prev, item.Weight)
+			}
+
+			prev = item.Weight
+		}
+	})
+}
