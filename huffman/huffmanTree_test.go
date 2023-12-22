11011909e153df521ab117464735115a9f9f78fa
Leonardpepa <leopepp4@gmail.com>
2023-12-22 22:11:54 +0200

    update test

--- huffman/huffmanTree_test.go
+++ huffman/huffmanTree_test.go
@@ -8,14 +8,14 @@ import (
 func TestCreatePriorityQueue(t *testing.T) {
 	t.Run("create priority queue from frequency table", func(t *testing.T) {
 		frequencies := map[rune]uint64{
-			'L': 42,
-			'K': 7,
 			'M': 24,
 			'C': 32,
-			'U': 37,
-			'Z': 2,
+			'K': 7,
 			'D': 42,
 			'E': 120,
+			'L': 42,
+			'U': 37,
+			'Z': 2,
 		}
 
 		pq := CreatePriorityQueue(frequencies)
@@ -30,5 +30,19 @@ func TestCreatePriorityQueue(t *testing.T) {
 
 			prev = item.Weight
 		}
+
+		heap.Push(&pq, CreateLeafNode('P', 163))
+		heap.Push(&pq, CreateLeafNode('0', 1))
+
+		item := heap.Pop(&pq).(*TreeNode)
+
+		if item.Weight != 1 {
+			t.Errorf("wrong item popped. expected 1, got %d", item.Weight)
+		}
+
+		if pq.Len() != 1 {
+			t.Errorf("wron priority queue size expected 1. got %d", pq.Len())
+		}
+
 	})
 }
