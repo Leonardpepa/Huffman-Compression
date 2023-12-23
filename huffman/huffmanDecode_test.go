41810f3605f1f8977c0b1a162ed8554f1d0c5d7c
Leonardpepa <leopepp4@gmail.com>
2023-12-23 22:50:35 +0200

    added test TestCreateTreeFromHeader

--- huffman/huffmanDecode_test.go
+++ huffman/huffmanDecode_test.go
@@ -2,6 +2,7 @@ package huffman
 
 import (
 	"huffmanCompression/bitstream"
+	"slices"
 	"testing"
 )
 
@@ -57,5 +58,52 @@ func TestDecodedText(t *testing.T) {
 		if *text != "KUL" {
 			t.Errorf("when decoding text expected KUL, got %s", *text)
 		}
+	})
+}
+
+func TestCreateTreeFromHeader(t *testing.T) {
+	t.Run("should create the tree by reading the header information", func(t *testing.T) {
+		frequencies := map[rune]uint64{
+			'Z': 2,
+			'K': 7,
+			'M': 24,
+			'C': 32,
+			'D': 42,
+			'L': 42,
+			'U': 37,
+			'E': 120,
+		}
+
+		root := CreateHuffmanTreeFromFrequencies(frequencies)
+
+		expected := make([]rune, 0)
+
+		getInorderChars(root, &expected)
+
+		writer := bitstream.CreateBitWriter()
+
+		size, err := encodeHuffmanHeaderInformation(root, PreOrder, &writer)
+
+		writer.Flush()
+
+		if err != nil {
+			t.Error(err)
+		}
+		reader := bitstream.CreateBitReader(writer.Bytes())
+
+		treeFromHeader, err := createTreeFromHeader(&reader, size)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		got := make([]rune, 0)
+
+		getInorderChars(treeFromHeader, &got)
+
+		if !slices.Equal(expected, got) {
+			t.Errorf("wrong tree created when reading the header")
+		}
+
 	})
 }
